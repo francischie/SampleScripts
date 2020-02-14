5178616631e01f29a1ed0dec7740c27e87f2e1ff
Francis <francis.marasigan@iherb.com>
2020-02-14 10:33:33 -0800

    updated scripts

--- LoadTest.js
+++ LoadTest.js
@@ -1,7 +1,10 @@
 import http from "k6/http";
 import { sleep } from "k6";
-
 export default function() {
-  http.get("http://checkout-legacyv2-api.checkout.svc/api/v2/Category/url");
+  let ressult = http.get("http://checkout-legacyv2-api.checkout.svc/api/v2/Order/lastFiveOrder");
+  check(res, {
+    "status was 200": (r) => r.status == 200,
+    "transaction time OK": (r) => r.timings.duration < 200
+  });
   sleep(1);
 };
