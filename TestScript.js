7ba9fbda96447f597ab7a94e0fc7f795d499c5f0
Francis <francis.marasigan@iherb.com>
2020-02-14 11:02:54 -0800

    fixed script

--- TestScript.js
+++ TestScript.js
@@ -12,7 +12,7 @@ export default function() {
   ];
 
   for(var i = 0; i < urls.length; i++) {
-    let res = http.get("http://checkout-legacyv2-api.checkout.svc/" + urls[i]);
+    let res = http.get("http://checkout-legacyv2-api.checkout.svc/api/v2/" + urls[i]);
     check(res, {
       "status was 200": (r) => r.status == 200,
       "transaction time OK": (r) => r.timings.duration < 200
