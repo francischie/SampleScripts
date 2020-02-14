fb7560da4bc03489886d108f06ba11a41b085bcc
Francis <francis.marasigan@iherb.com>
2020-02-14 10:38:39 -0800

    fixed script

--- LoadTest.js
+++ LoadTest.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
-import { sleep } from "k6";
-import { check } from "k6";
+import { sleep, check } from "k6";
+
 export default function() {
   let res = http.get("http://checkout-legacyv2-api.checkout.svc/api/v2/Order/lastFiveOrder");
   check(res, {
