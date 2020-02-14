90f74c9080708dbb02f78678452cb8e75dc789d6
Francis <francis.marasigan@iherb.com>
2020-02-14 10:35:00 -0800

    fixed script

--- LoadTest.js
+++ LoadTest.js
@@ -1,7 +1,7 @@
 import http from "k6/http";
 import { sleep } from "k6";
 export default function() {
-  let ressult = http.get("http://checkout-legacyv2-api.checkout.svc/api/v2/Order/lastFiveOrder");
+  let res = http.get("http://checkout-legacyv2-api.checkout.svc/api/v2/Order/lastFiveOrder");
   check(res, {
     "status was 200": (r) => r.status == 200,
     "transaction time OK": (r) => r.timings.duration < 200
